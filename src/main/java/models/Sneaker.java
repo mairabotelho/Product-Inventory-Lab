d5ccc12e9d4eb31216a7bbde885b030a84f957d0
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-3.local>
2019-11-15 13:20:47 -0500

    sneaker

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -2,6 +2,16 @@ package models;
 
 public class Sneaker {
 
+    private int id;
+    private String name;
+    private String brand;
+    private String sport;
+    private float size;
+    private int quantity;
+    private float price;
+
+
+
 
 }
 
